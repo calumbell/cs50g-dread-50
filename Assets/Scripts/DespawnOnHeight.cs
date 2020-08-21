1402c053f0b0f65ac9cb157ca179ee01cef773c0
Calum Bell <calumabell@googlemail.com>
2020-08-21 11:55:21 +0100

    whispers end at game over

--- Assets/Scripts/DespawnOnHeight.cs
+++ Assets/Scripts/DespawnOnHeight.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DespawnOnHeight : MonoBehaviour
-{
+public class DespawnOnHeight : MonoBehaviour {
     public float despawnHeight;
+    private GameObject audioSource;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        audioSource = GameObject.FindGameObjectWithTag("Audio");
     }
 
     // Update is called once per frame
     void Update() {
         if (transform.position.y < despawnHeight) {
+            Destroy(audioSource);
             SceneManager.LoadScene("GameOver");
         }
     }
